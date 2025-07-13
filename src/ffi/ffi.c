98c0dc9a4007a1c4c011b07290a63d5200e3f921
t81dev <sansdisk0134@icloud.com>
2025-07-13 10:03:06 -0400

    Update ffi.c

--- src/ffi/ffi.c
+++ src/ffi/ffi.c
@@ -1,1 +1,10 @@
+void call_ffi(uint8_t func_id) {
+    switch (func_id) {
+        case 0x01:
+            printf("Hello from C FFI!\n");
+            break;
+        default:
+            printf("Unknown FFI function: %d\n", func_id);
+    }
+}
 
