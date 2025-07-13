d191952ede54f640421e57a0fc0c4a3112860e25
t81dev <sansdisk0134@icloud.com>
2025-07-13 10:43:18 -0400

    Update symbolic.c

--- src/symbolic/symbolic.c
+++ src/symbolic/symbolic.c
@@ -1,1 +1,35 @@
+#include "symbolic.h"
+#include <stdio.h>
+
+static Symbol symbol_table[MAX_SYMBOLS];
+
+void symbolic_init(void) {
+    for (int i = 0; i < MAX_SYMBOLS; ++i) {
+        symbol_table[i].id = 0;
+        symbol_table[i].value = 0;
+    }
+}
+
+void symbol_define(uint8_t id, uint8_t value) {
+    symbol_table[id].id = id;
+    symbol_table[id].value = value;
+    printf("[Symbolic] Defined symbol 0x%02X = %d\n", id, value);
+}
+
+void symbol_apply(uint8_t id) {
+    printf("[Symbolic] Applied symbol 0x%02X = %d\n", id, symbol_table[id].value);
+}
+
+void symbol_print_all(void) {
+    printf("[Symbolic] Symbol Table:\n");
+    for (int i = 0; i < MAX_SYMBOLS; ++i) {
+        if (symbol_table[i].id != 0) {
+            printf("  Symbol 0x%02X = %d\n", symbol_table[i].id, symbol_table[i].value);
+        }
+    }
+}
+
+void meta_reflect(void) {
+    printf("[Meta] Reflection not yet implemented.\n");
+}
 
