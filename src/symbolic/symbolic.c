4d577364835ed9135320022b840a1f63114ddc88
t81dev <sansdisk0134@icloud.com>
2025-07-13 10:46:39 -0400

    Update symbolic.c

--- src/symbolic/symbolic.c
+++ src/symbolic/symbolic.c
@@ -20,16 +20,6 @@ void symbol_apply(uint8_t id) {
     printf("[Symbolic] Applied symbol 0x%02X = %d\n", id, symbol_table[id].value);
 }
 
-void symbol_print_all(void) {
-    printf("[Symbolic] Symbol Table:\n");
-    for (int i = 0; i < MAX_SYMBOLS; ++i) {
-        if (symbol_table[i].id != 0) {
-            printf("  Symbol 0x%02X = %d\n", symbol_table[i].id, symbol_table[i].value);
-        }
-    }
-}
-
 void meta_reflect(void) {
     printf("[Meta] Reflection not yet implemented.\n");
 }
-
