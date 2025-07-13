d0adf3007b6d2cbc2e5b58f4ec6a0d169defd002
t81dev <sansdisk0134@icloud.com>
2025-07-13 10:08:30 -0400

    Update ffi_python_plugin.py

--- examples/ffi_python_plugin.py
+++ examples/ffi_python_plugin.py
@@ -1,1 +1,7 @@
-Example external plugin in Python
+# ffi_python_plugin.py
+
+def say_hello():
+    print("[Python] Hello from Python FFI!")
+
+def print_register0():
+    print("[Python] This could access VM state if passed in.")
